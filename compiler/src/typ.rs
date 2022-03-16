fa33f06fc6ab3d69fe8a85881143e4c73772a662
justinfargnoli <justinfargnoli@gmail.com>
2022-03-16 12:56:52 -0400

    Add partial implementation of type checking `Nat`

--- compiler/src/typ.rs
+++ compiler/src/typ.rs
@@ -20,6 +20,22 @@ pub mod check {
                         declarations: Vec::new(),
                     }
                 }
+
+                pub fn lookup_inductive(&self, name: &str) -> Inductive {
+                    for declaration in &self.declarations {
+                        if let Declaration::Inductive(inductive_name, inductive) = declaration {
+                            if inductive_name == name {
+                                return inductive.clone();
+                            }
+                        }
+                    }
+                    panic!()
+                }
+
+                pub fn push_inductive(&mut self, inductive: Inductive) {
+                    self.declarations
+                        .push(Declaration::Inductive(inductive.name.clone(), inductive))
+                }
             }
 
             enum Declaration {
@@ -48,8 +64,12 @@ pub mod check {
                     }
                 }
 
-                pub fn push_declaration(&mut self, name: Name, body: Option<Term>, typ: Term) {
-                    self.declarations.push(Declaration { name, body, typ })
+                pub fn push_declaration(&mut self, name: Name, typ: Term) {
+                    self.declarations.push(Declaration {
+                        name,
+                        body: None,
+                        typ,
+                    })
                 }
 
                 pub fn pop_declaration(&mut self) {
@@ -67,7 +87,7 @@ pub mod check {
     }
 
     pub fn type_check_hir(hir: &HIR) {
-        let global = global::Environment::new();
+        let mut global = global::Environment::new();
         let mut local = local::Environment::new();
         for declaration in hir.declarations.iter() {
             match declaration {
@@ -75,12 +95,17 @@ pub mod check {
                     type_check_term(&global, &mut local, term);
                     // TODO: add this to the global environment
                 }
-                Declaration::Inductive(inductive) => {
-                    type_check_inductive(&global, &inductive)
-                    // TODO: add this to the global environment
-                }
+                Declaration::Inductive(inductive) => type_check_inductive(&mut global, &inductive),
             }
         }
+    }
+
+    pub fn type_check_fresh_term(term: &Term) -> Term {
+        type_check_term(
+            &global::Environment::new(),
+            &mut local::Environment::new(),
+            term,
+        )
     }
 
     // assert when type checking fails
@@ -107,27 +132,27 @@ pub mod check {
                 }
             }
             Term::DependentProduct {
-                parameter_name: name,
-                parameter_type: from_type,
-                return_type: to_type,
+                parameter_name,
+                parameter_type,
+                return_type,
             } => {
-                let from_type_type = type_check_term(global, local, from_type);
-                let from_type_universe = match from_type_type {
+                let parameter_type_type = type_check_term(global, local, parameter_type);
+                let parameter_type_universe = match parameter_type_type {
                     Term::Sort(ref universe) => universe,
-                    _ => panic!(),
+                    _ => panic!("{:#?}", parameter_type),
                 };
 
-                local.push_declaration(name.clone(), None, from_type_type.clone());
-                let to_type_type = type_check_term(global, local, to_type);
-                let to_type_universe = match to_type_type {
+                local.push_declaration(parameter_name.clone(), parameter_type_type.clone());
+                let return_type_type = type_check_term(global, local, return_type);
+                let return_type_universe = match return_type_type {
                     Term::Sort(ref universe) => universe,
-                    _ => panic!(),
+                    _ => panic!("{:#?}", return_type),
                 };
                 local.pop_declaration();
 
                 Term::Sort(Universe::sort_of_product(
-                    from_type_universe,
-                    to_type_universe,
+                    parameter_type_universe,
+                    return_type_universe,
                 ))
             }
             Term::Lambda {
@@ -137,7 +162,7 @@ pub mod check {
             } => {
                 type_check_term(global, local, parameter_type);
 
-                local.push_declaration(parameter_name.clone(), None, (**parameter_type).clone());
+                local.push_declaration(parameter_name.clone(), (**parameter_type).clone());
                 let body_type = type_check_term(global, local, body);
                 local.pop_declaration();
 
@@ -147,52 +172,104 @@ pub mod check {
                     return_type: Box::new(body_type),
                 }
             }
-            _ => todo!(),
+            Term::Inductive(identifier, _) => {
+                let _inductive = global.lookup_inductive(identifier);
+
+                // TODO: handle universes
+                term.clone()
+            }
+            _ => todo!("{:#?}", term),
+        }
+    }
+
+    pub fn type_check_fresh_inductive(inductive: &Inductive) {
+        type_check_inductive(&mut global::Environment::new(), inductive);
+    }
+
+    fn well_formed_arity(arity: &Term) {
+        match arity {
+            Term::DependentProduct { return_type, .. } => well_formed_arity(&*return_type),
+            Term::Sort(_) => (),
+            _ => panic!("arity is not well formed"),
         }
     }
 
     // assert when type checking fails
     // TODO: return error messages using ariadne.
-    fn type_check_inductive(global: &global::Environment, inductive: &Inductive) {
-        todo!()
+    fn type_check_inductive(global: &mut global::Environment, inductive: &Inductive) {
+        well_formed_arity(&inductive.arity);
+        type_check_fresh_term(&inductive.arity);
+
+        global.push_inductive(inductive.clone());
+        let mut constructor_sorts = inductive.constructors.iter().map(|constructor| {
+            type_check_term(global, &mut local::Environment::new(), &constructor.typ)
+        });
+        if let Some(first_constructor_sort) = constructor_sorts.next() {
+            assert!(constructor_sorts
+                .all(|constructor_sort| constructor_sort == first_constructor_sort))
+        }
     }
 
     #[cfg(test)]
     mod tests {
         use super::*;
-        use crate::hir::ir::{universe::Level, Name};
-
-        fn test_type_check_declaration(declaration: Declaration) {
-            let mut hir = HIR::new();
-            hir.declarations.push(declaration);
-            type_check_hir(&hir);
-        }
+        use crate::hir::ir::{universe::UniverseInstance, Constructor, Name};
 
         #[test]
         fn identity_function() {
-            test_type_check_declaration(Declaration::Constant(Term::Lambda {
-                parameter_name: Name::Named("a".to_string()),
-                parameter_type: Box::new(Term::Sort(Universe::build_one(Expression::build(
-                    Level::Set,
-                    false,
-                )))),
-                body: Box::new(Term::DeBruijnIndex(0)),
-            }))
+            let parameter_type = Box::new(Term::Sort(Universe::build_one(Expression::set())));
+            assert_eq!(
+                type_check_fresh_term(&Term::Lambda {
+                    parameter_name: Name::Named("a".to_string()),
+                    parameter_type: parameter_type.clone(),
+                    body: Box::new(Term::DeBruijnIndex(0)),
+                }),
+                Term::DependentProduct {
+                    parameter_name: Name::Named("a".to_string()),
+                    parameter_type: parameter_type.clone(),
+                    return_type: parameter_type,
+                }
+            )
         }
 
         #[test]
         #[should_panic]
         fn identity_function_malformed() {
-            test_type_check_declaration(Declaration::Constant(Term::Lambda {
+            type_check_fresh_term(&Term::Lambda {
                 parameter_name: Name::Named("a".to_string()),
-                parameter_type: Box::new(Term::Sort(Universe::build_one(Expression::build(
-                    Level::Set,
-                    false,
-                )))),
+                parameter_type: Box::new(Term::Sort(Universe::build_one(Expression::set()))),
                 body: Box::new(Term::DeBruijnIndex(1)),
-            }))
+            });
+        }
+
+        #[test]
+        fn nat() {
+            let natural = "Natural".to_string();
+            type_check_fresh_inductive(&Inductive {
+                name: natural.clone(),
+                parameters: Vec::new(),
+                arity: Term::Sort(Universe::build_one(Expression::set())),
+                constructors: vec![
+                    Constructor {
+                        name: "Zero".to_string(),
+                        typ: Term::Inductive(natural.clone(), UniverseInstance::empty()),
+                    },
+                    Constructor {
+                        name: "Successor".to_string(),
+                        typ: Term::DependentProduct {
+                            parameter_name: Name::Anonymous,
+                            parameter_type: Box::new(Term::Inductive(
+                                natural.clone(),
+                                UniverseInstance::empty(),
+                            )),
+                            return_type: Box::new(Term::Inductive(
+                                natural.clone(),
+                                UniverseInstance::empty(),
+                            )),
+                        },
+                    },
+                ],
+            })
         }
     }
 }
-
-mod pass {}
