68fed69975726bb04146949bae45e23ad34d127b
justinfargnoli <justinfargnoli@gmail.com>
2022-04-25 12:51:02 -0400

    Cleanup `lib.rs`

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -3,10 +3,3 @@
 pub mod codegen;
 pub mod hir;
 pub mod typ;
-
-// TODO: termination checking
-// TODO: make sure functions aren't named 'main'
-// TODO: implement currying
-
-#[cfg(test)]
-mod tests {}
